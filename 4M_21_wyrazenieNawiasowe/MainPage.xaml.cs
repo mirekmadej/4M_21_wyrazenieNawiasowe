e7e3ebfc759b3dea516693e30b14b19bcd25109e
Mirek Madej <m.madej@zsp3zamosc.pl>
2024-03-06 10:00:56 +0100

    pełny projekt

--- 4M_21_wyrazenieNawiasowe/MainPage.xaml.cs
+++ 4M_21_wyrazenieNawiasowe/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace _4M_21_wyrazenieNawiasowe
+﻿using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Globalization;
+namespace _4M_21_wyrazenieNawiasowe
 {
     public partial class MainPage : ContentPage
     {
@@ -6,7 +9,41 @@
 
         public MainPage()
         {
+            string connStr = "server=localhost;user=root;database=pojazd;port=3306;password=";
+            MySqlConnection con = new MySqlConnection(connStr);
+            string q = "SELECT marka FROM pojazd";
+            con.Open();
+            MySqlCommand com = new MySqlCommand(q, con);
+            MySqlDataReader rdr = com.ExecuteReader();
+            var marki = new List<string>();
+            while (rdr.Read())
+            {
+                marki.Add(rdr[0].ToString());
+            }
+            rdr.Close();
+            con.Close();
+
             InitializeComponent();
+
+            var ht = new HashSet<string>();
+            foreach (var s in marki)
+                ht.Add(s);
+            lblLiczbaMarek.Text = $"Liczba unikalnych marek {ht.Count}";
+            var slownik = new Dictionary<string, int>();
+            foreach(var s in marki)
+            {
+                if (!slownik.ContainsKey(s))
+                    slownik.Add(s, 1);
+                else
+                    slownik[s]++;
+            }
+            string m = "";
+            foreach (var s in slownik)
+            {
+                m += s.Key + " " + s.Value + "\n";
+            }
+
+            lblMarki.Text = m;
         }
 
         private void onCompleted(object sender, EventArgs e)
@@ -32,6 +69,30 @@
             else
                 lblStatus.Text = "Status wyrażenia: niepoprawne";
         }
+
+        private void onCompletedKierunki(object sender, EventArgs e)
+        {
+            string s = entKierunki.Text.ToUpper();
+            var ht = new HashSet<string>();
+            int x=0, y=0;
+            foreach (char c in s)
+            {
+                switch (c)
+                {
+                    case 'N': y++; break;
+                    case 'S': y--; break;
+                    case 'E': x++; break;
+                    case 'W': x--; break;                    
+                }
+                ht.Add(x.ToString()+"*"+y.ToString());
+            }
+            lblKierunki.Text = $"Liczba unikalnych pól: {ht.Count}";
+
+        }
+
+
+
+
     }
 
 }
